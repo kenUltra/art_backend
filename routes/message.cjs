8657321268dba52cea3cd7f05e02e3101ef6b281
Ken Mikaelson <mikaelsoncedric@gmail.com>
2025-11-13 21:59:03 -0800

    apply change

--- routes/message.cjs
+++ routes/message.cjs
@@ -7,7 +7,7 @@ const roles = require("../middleware/checkRole.cjs");
 routerExpress.route("/posts/:uuid")
         .get(roles(userRoles.User, userRoles.betaUser, userRoles.Executive, userRoles.Manager),messageController.getMessages)
         .post(messageController.postMessage)
-        .delete(roles(userRoles.betaUser, userRoles.Executive, userRoles.Manager), messageController.deleteMessage)
+        .delete(roles(userRoles.betaUser, userRoles.User, userRoles.Manager), messageController.deleteMessage)
         .patch(roles(userRoles.betaUser, userRoles.User, userRoles.Executive, userRoles.Manager), messageController.likeMessage);
 
 module.exports = routerExpress;
